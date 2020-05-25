76a43b7d2aca516310882f61eb3d67ef7ad35960
Ali Amid <ali.amid32@yahoo.com>
2020-05-25 17:48:26 +0430

    adding meaningful comments

--- src/Countingsort.java
+++ src/Countingsort.java
@@ -1,42 +1,43 @@
-import java.util.Arrays;
-
 public class Countingsort {
 
-    private int sizecountarray = 0;
+    private int SizeOfCountArray = 0;
+    private int[] SortedArray;
 
-   public void Countingsort(int[] Array){
+    public int[] CountingSort(int[] Array) {
 
         CountArray(Array);
-    }
-    private void CountArray(int[] Array){
-       for(int i =0; i<=Array.length-1; i++){
-           if(Array[i]>this.sizecountarray){
-               this.sizecountarray=Array[i];
-           }
-       }
-       int[] countarray = new int[this.sizecountarray+1];
-       Sort(Array, countarray);
+        return this.SortedArray;
+
     }
 
-    private void Sort(int[] Array, int[]countarray){
-       int countarrayindex = 0;
-       for (int i=0; i<=Array.length-1; i++){
-
-           countarrayindex = Array[i];
-           countarray[countarrayindex]= countarray[countarrayindex]+1;
-       }
-       for (int i=1; i<=countarray.length-1;i++){
-           countarray[i]=countarray[i-1] + countarray[i];}
-       int[] SortedArray = new int[Array.length];
-       for (int i=0; i<=Array.length-1;i++ ){
-           SortedArray[(countarray[Array[i]])-1] = Array[i];
-           countarray[Array[i]]--;}
-        System.out.println(Arrays.toString(SortedArray));
-
-
-
-
-       }
+    // this method creates a CountArray according to the maximum integer in the unsorted Array
+    private void CountArray(int[] Array) {
+        for (int i = 0; i <= Array.length - 1; i++) {
+            if (Array[i] > this.SizeOfCountArray) {
+                this.SizeOfCountArray = Array[i];
+            }
+        }
+        int[] CountArray = new int[this.SizeOfCountArray + 1];
+        Sort(Array, CountArray);
     }
+
+    private void Sort(int[] Array, int[] CountArray) {
+        int CountArrayIndex = 0;
+        for (int i = 0; i <= Array.length - 1; i++) {
+
+            CountArrayIndex = Array[i];
+            CountArray[CountArrayIndex] = CountArray[CountArrayIndex] + 1;
+        }
+        for (int i = 1; i <= CountArray.length - 1; i++) {
+            CountArray[i] = CountArray[i - 1] + CountArray[i];
+        }
+        SortedArray = new int[Array.length];
+        for (int i = 0; i <= Array.length - 1; i++) {
+            SortedArray[(CountArray[Array[i]]) - 1] = Array[i];
+            CountArray[Array[i]]--;
+        }
+
+    }
+}
 
 
--- src/Main.java
+++ src/Main.java
@@ -3,22 +3,24 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-//        // make a new instance of Quicksort class
-//        Quicksort quicksort = new Quicksort();
-//        // Define an unsorted array
-//        int[] array = {5,5,5,4,8,0,0,8,8,1000,2,3,4,0,0,4,4,4};
-//        // call quicksort method and pass the array
-//        quicksort.quicksort(array);
-//        // print out the sorted array on the terminal
-//        System.out.println(Arrays.toString(array));
+        // make a new instance of Quicksort class
+        Quicksort quicksort = new Quicksort();
+        // Define an unsorted array
+        int[] array1 = {5,5,5,4,8,0,0,8,8,1000,2,3,4,0,0,4,4,4};
+        // call quicksort method and pass the array
+        quicksort.quicksort(array1);
+        // print out the sorted array on the terminal
+        System.out.println(Arrays.toString(array1));
 
+
+        // make a new instance of Countingsort class
         Countingsort countingsort = new Countingsort();
-        int[] array = {5,6,4,3,10,10,100,0,0};
-        countingsort.Countingsort(array);
-
-
-
-
+        // Define an unsorted array
+        int[] array2 = {5,6,4,3,10,10,100,0,0,1000};
+        // call Countingsort method and pass the array. this method returns the sorted array
+        int[] SortedArray = countingsort.CountingSort(array2);
+        //Print the SortedArray
+        System.out.println(Arrays.toString(SortedArray));
 
     }
 }
